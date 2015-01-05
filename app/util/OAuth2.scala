ef4852fe544362a3bea6e9f2661b9539db531007
Trevor Allred <trevorallred@gmail.com>
2015-01-04 20:37:56 -0800

    change to LDS Connect (unsuccessfully)

--- app/util/OAuth2.scala
+++ app/util/OAuth2.scala
@@ -10,24 +10,30 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class OAuth2(application: Application) {
-  lazy val githubAuthId = application.configuration.getString("github.client.id").get
-  lazy val githubAuthSecret = application.configuration.getString("github.client.secret").get
+  lazy val ldsAuthId = application.configuration.getString("lds.client.id").get
+  lazy val ldsAuthSecret = application.configuration.getString("lds.client.secret").get
 
   def getAuthorizationUrl(redirectUri: String, scope: String, state: String): String = {
-    val baseUrl = application.configuration.getString("github.redirect.url").get
-    baseUrl.format(githubAuthId, redirectUri, scope, state)
+    val authEndpoint = application.configuration.getString("lds.redirect.url").get
+    val responseType = "code"
+    authEndpoint.format(responseType, ldsAuthId, redirectUri, scope, state)
   }
 
   def getToken(code: String): Future[String] = {
-    val tokenResponse = WS.url("https://github.com/login/oauth/access_token")(application).
-      withQueryString("client_id" -> githubAuthId,
-        "client_secret" -> githubAuthSecret,
+    val tokenEndpoint = application.configuration.getString("lds.token.endpoint").get
+    val tokenResponse = WS.url(tokenEndpoint)(application).
+      withQueryString("client_id" -> ldsAuthId,
+        "client_secret" -> ldsAuthSecret,
         "code" -> code).
       withHeaders(HeaderNames.ACCEPT -> MimeTypes.JSON).
       post(Results.EmptyContent())
 
     tokenResponse.flatMap { response =>
-      (response.json \ "access_token").asOpt[String].fold(Future.failed[String](new IllegalStateException("Sod off!"))) { accessToken =>
+      println("Status: " + response.status)
+      println(response.body)
+      (response.json \ "access_token").asOpt[String].fold(
+        Future.failed[String](new IllegalStateException(response.json.toString))
+      ) { accessToken =>
         Future.successful(accessToken)
       }
     }
@@ -51,7 +57,7 @@ object OAuth2 extends Controller {
         }
       }
       else {
-        Future.successful(BadRequest("Invalid github login"))
+        Future.successful(BadRequest("Invalid lds login"))
       }
     }).getOrElse(Future.successful(BadRequest("No parameters supplied")))
   }
@@ -59,7 +65,7 @@ object OAuth2 extends Controller {
   def success() = Action.async { request =>
     implicit val app = Play.current
     request.session.get("oauth-token").fold(Future.successful(Unauthorized("No way Jose"))) { authToken =>
-      WS.url("https://api.github.com/user/repos").
+      WS.url("https://ldsconnect.org/api/ldsorg/me").
         withHeaders(HeaderNames.AUTHORIZATION -> s"token $authToken").
         get().map { response =>
           Ok(response.json)
