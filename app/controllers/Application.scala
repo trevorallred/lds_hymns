b33a739db8f9f6311a139fa3d2d85a8db863ddf3
Trevor Allred <trevorallred@gmail.com>
2015-01-04 17:28:48 -0800

    add test oauth for github

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,22 +1,34 @@
 package controllers
 
+import java.util.UUID
+
 import play.api._
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits._
 
 import scala.concurrent.Future
-import scala.util.control.NonFatal
+import util.OAuth2
 
 object Application extends Controller {
 
-  def index = Action.async {
+  def index = Action { implicit request =>
+    val oauth2 = new OAuth2(Play.current)
+    val callbackUrl = util.routes.OAuth2.callback(None, None).absoluteURL()
+    val scope = "repo" // github scope - request repo access
+  val state = UUID.randomUUID().toString // random confirmation string
+  val redirectUrl = oauth2.getAuthorizationUrl(callbackUrl, scope, state)
+    Ok(views.html.index("Your new application is ready.", redirectUrl)).
+      withSession("oauth-state" -> state)
+  }
+
+  def mobile = Action.async {
     Future {
       Ok(views.html.mobile())
     }
   }
 
   def jsonHymns = Action {
-//    val hymns = None;
+    //    val hymns = None;
     Ok(views.html.hymns.list(???))
   }
 
