20b1dde2885b99160befeef24c1f8da5d99d9c4a
Trevor Allred <trevorallred@gmail.com>
2015-01-08 08:34:42 -0800

    got oauth working finally after weeks

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,9 +14,10 @@ object Application extends Controller {
   def index = Action { implicit request =>
     val oauth2 = new OAuth2(Play.current)
     val callbackUrl = util.routes.OAuth2.callback(None, None).absoluteURL()
-    val scope = "repo" // github scope - request repo access
-  val state = UUID.randomUUID().toString // random confirmation string
-  val redirectUrl = oauth2.getAuthorizationUrl(callbackUrl, scope, state)
+    val scope = "todo" // TODO figure out scope for LDS Connect
+    val state = UUID.randomUUID().toString // random confirmation string
+    val redirectUrl = oauth2.getAuthorizationUrl(callbackUrl, "code", scope, state)
+    println(redirectUrl)
     Ok(views.html.index("Your new application is ready.", redirectUrl)).
       withSession("oauth-state" -> state)
   }
--- app/util/OAuth2.scala
+++ app/util/OAuth2.scala
@@ -3,7 +3,7 @@ package util
 import play.api.Application
 import play.api.Play
 import play.api.http.{MimeTypes, HeaderNames}
-import play.api.libs.ws.WS
+import play.api.libs.ws.{WSAuthScheme, WS}
 import play.api.mvc.{Results, Action, Controller}
 
 import scala.concurrent.Future
@@ -13,28 +13,30 @@ class OAuth2(application: Application) {
   lazy val ldsAuthId = application.configuration.getString("lds.client.id").get
   lazy val ldsAuthSecret = application.configuration.getString("lds.client.secret").get
 
-  def getAuthorizationUrl(redirectUri: String, scope: String, state: String): String = {
+  def getAuthorizationUrl(redirectUri: String, responseType: String, scope: String, state: String): String = {
     val authEndpoint = application.configuration.getString("lds.redirect.url").get
-    val responseType = "code"
     authEndpoint.format(responseType, ldsAuthId, redirectUri, scope, state)
   }
 
   def getToken(code: String): Future[String] = {
     val tokenEndpoint = application.configuration.getString("lds.token.endpoint").get
     val tokenResponse = WS.url(tokenEndpoint)(application).
-      withQueryString("client_id" -> ldsAuthId,
-        "client_secret" -> ldsAuthSecret,
-        "code" -> code).
-      withHeaders(HeaderNames.ACCEPT -> MimeTypes.JSON).
-      post(Results.EmptyContent())
+      withAuth(ldsAuthId, ldsAuthSecret, WSAuthScheme.BASIC).
+      post(Map(
+      "grant_type" -> Seq("authorization_code"),
+      "redirect_uri" -> Seq("http://lds.trevorallred.com:9000/_oauth-callback"),
+      "code" -> Seq(code)
+    ))
 
     tokenResponse.flatMap { response =>
-      println("Status: " + response.status)
-      println(response.body)
-      (response.json \ "access_token").asOpt[String].fold(
-        Future.failed[String](new IllegalStateException(response.json.toString))
-      ) { accessToken =>
-        Future.successful(accessToken)
+      if (response.status == 200) {
+        (response.json \ "access_token").asOpt[String].fold(
+          Future.failed[String](new IllegalStateException(response.json.toString))
+        ) { accessToken =>
+          Future.successful(accessToken)
+        }
+      } else {
+        Future.failed[String](new IllegalStateException(response.status + ": " + response.body))
       }
     }
   }
@@ -55,21 +57,22 @@ object OAuth2 extends Controller {
         }.recover {
           case ex: IllegalStateException => Unauthorized(ex.getMessage)
         }
+      } else {
+        Future.successful(BadRequest("Your session has changed. Try again."))
       }
-      else {
-        Future.successful(BadRequest("Invalid lds login"))
-      }
-    }).getOrElse(Future.successful(BadRequest("No parameters supplied")))
+    }).getOrElse(
+        Future.successful(BadRequest("Missing either code or state or session state parameters"))
+      )
   }
 
   def success() = Action.async { request =>
     implicit val app = Play.current
     request.session.get("oauth-token").fold(Future.successful(Unauthorized("No way Jose"))) { authToken =>
       WS.url("https://ldsconnect.org/api/ldsorg/me").
-        withHeaders(HeaderNames.AUTHORIZATION -> s"token $authToken").
+        withHeaders(HeaderNames.AUTHORIZATION -> s"Bearer $authToken").
         get().map { response =>
-          Ok(response.json)
-        }
+        Ok(response.json)
+      }
     }
   }
 }
